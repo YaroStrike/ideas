fe1aee87a91b671496df6f790943572734e89414
= <=>
2025-01-14 15:27:31 +1100

    smth

---
+++ on-tg-api/compare.py
@@ -0,0 +1,6 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final[str] = '6493157409:AAElg1VuCGMYuEoaQTJOTlqheiDyWXrtcSw'
+BOT_USERNAME: Final[str] = '@chatsweep_bot'
--- on-tg-api/main.py
+++ on-tg-api/main.py
@@ -11,5 +11,4 @@ async def main() -> None:
     await app.run_polling()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
