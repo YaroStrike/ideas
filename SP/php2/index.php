710cf41bb9d54c2f529f7846eef4a51b997fbf02
= <=>
2025-02-13 10:00:01 +1100

    оп

--- SP/php2/index.php
+++ SP/php2/index.php
@@ -2,8 +2,8 @@
 $handle = fopen("test.txt", "rt");
 if ($handle) {
     $contents = '';
-    while (!feof($handle)) {
-        $contents .= fread($handle, 4096);
+    while (!feof($handle)) { // пока (!прочитан($файл)) 
+        $contents .= fread($handle, 4096); // буфер содержимого файла
     }
     fclose($handle);
     echo $contents;
